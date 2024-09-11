01373c6f6a23cb386aae5a829cc09c7c650a5891
arun balan <arunbalan303@gmail.com>
2024-09-11 17:06:44 +0530

    test

---
+++ src/imageUpload/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Uppy from '@uppy/core';
+import { DragDrop } from '@uppy/react';
+const index = () => {
+  return (
+    <>
+      <DragDrop  />
+    </>
+  )
+}
+
+export default index
